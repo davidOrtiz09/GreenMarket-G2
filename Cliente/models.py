77d4fd3e4c7da0271b1c7427381498dd210e8d68
David Ortiz <davideduardo.ortiz@gmail.com>
2017-09-28 00:09:44 -0500

    GM-99: Cambio de formato en el archivo de models cliente

--- Cliente/models.py
+++ Cliente/models.py
@@ -8,6 +8,7 @@ from django.db import models
 # -*-
 from django import forms
 
+
 class Cliente(models.Model):
     TIPO_DOCUMENTOS = (
         ('CC', 'Cédula de Ciudadanía'),
@@ -16,7 +17,7 @@ class Cliente(models.Model):
         ('RC', 'Registro Civil')
     )
 
-    ##TODO: Cargar ciudades y departamentos
+    # TODO: Cargar ciudades y departamentos
 
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=150, unique=False, null=False, blank=False)
