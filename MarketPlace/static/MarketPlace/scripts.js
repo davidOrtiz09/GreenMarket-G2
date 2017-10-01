8125cead36755644afeddb344cdca31712a427c8
Camilo Tangarife <jc.tangarife1927@gmail.com>
2017-10-01 00:28:16 -0500

    Ajuste en la maquetación y funcionalidad javascript para aumentar y disminuir cantidades. 1 hora

---
+++ MarketPlace/static/MarketPlace/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    setShoppingCartListeners();
+});
+
+function setShoppingCartListeners(){
+    $('.product-minus').click(function(){
+        var input = $(this).parent().parent().find('input.product-quantity');
+        var value = parseInt(input.val());
+        if(value > 0){
+            input.val(value-1);
+        }
+    });
+
+    $('.product-plus').click(function(){
+        var input = $(this).parent().parent().find('input.product-quantity');
+        var value = parseInt(input.val());
+        input.val(value+1);
+    });
+}
