f583b7787a65a4a9db53f6fa51a5424ff1996094
Camilo Tangarife <jc.tangarife1927@gmail.com>
2017-10-01 15:53:28 -0500

    Se mejora la funcionalidad javascript para añadir nuevos elementos al carrito. Se manejará la añadida de productos via django y no via javascript. 1h 30 min

--- Cliente/views.py
+++ Cliente/views.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
-from django.http import JsonResponse
+from django.contrib import messages
 
 
 class Index(View):
     def get(self, request):
         return render(request, 'Cliente/index.html', {})
+
+
+class UpdateShoppingCart(View):
+    def post(self, request):
+        messages.add_message(request, messages.SUCCESS, 'El producto se agregó al carrito satisfactoriamente')
+        return redirect(request.META.get('HTTP_REFERER', '/'))
--- MarketPlace/static/MarketPlace/scripts.js
+++ MarketPlace/static/MarketPlace/scripts.js
@@ -8,6 +8,7 @@ function setShoppingCartListeners(){
         var value = parseInt(input.val());
         if(value > 0){
             input.val(value-1);
+            input.change();
         }
     });
 
@@ -15,9 +16,20 @@ function setShoppingCartListeners(){
         var input = $(this).parent().parent().find('input.product-quantity');
         var value = parseInt(input.val());
         input.val(value+1);
+        input.change();
     });
 
-    $('.product-add').click(function(){
-        $('#modal-producto-agregado').modal('show')
+    $('input.product-quantity').change(function(){
+        var value = $(this).val();
+        var minus = $(this).parent().find('.product-minus');
+        var addToCart = $(this).parent().parent().find('.add-to-cart');
+        if(value > 0){
+            addToCart.attr('disabled', false);
+            minus.attr('disabled', false);
+        }
+        else{
+            addToCart.attr('disabled', true);
+            minus.attr('disabled', true);
+        }
     });
 }
