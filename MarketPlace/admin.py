b2ec58cbc8b24d546fc1d6b885ff1c3793a26980
Alfonso Ardila <alfyes@hotmail.com>
2017-10-02 02:33:23 -0500

    feat: Se agrega al módulo de Administación de Django algunos modelos.
    Se agregaron los modelos de: Categoria, producto, oferta, oferta producto.
    
    Esto se realizó con el fin de poder tener una forma fácil de agregar datos para
    pruebas mientras se implementan las funcionalidades necesarias para crear datos
    de estos modelos.

--- MarketPlace/admin.py
+++ MarketPlace/admin.py
@@ -4,3 +4,30 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+from MarketPlace.models import Categoria, Producto, Oferta, oferta_producto
+
+
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre')
+    list_display_links = ('id', 'nombre')
+    search_fields = ('nombre',)
+
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre')
+    list_display_links = ('id', 'nombre')
+    search_fields = ('nombre',)
+
+class OfertaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'productor_id', 'fecha')
+    list_display_links = ('id',)
+    search_fields = ('nombre',)
+
+class oferta_productoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'fk_oferta', 'fk_producto', 'estado', 'cantidad_aceptada', 'cantidad_vendida', 'precio')
+    list_display_links = ('id',)
+    search_fields = ('estado',)
+
+admin.site.register(Categoria, CategoriaAdmin)
+admin.site.register(Producto, ProductoAdmin)
+admin.site.register(Oferta, OfertaAdmin)
+admin.site.register(oferta_producto, oferta_productoAdmin)
