03a73138584e880ea6c32f3e72dcee112adc735f
Camilo Tangarife <jc.tangarife1927@gmail.com>
2017-10-01 22:38:34 -0500

    Funcionalidad para añadir el carrito de compras los productoss en las variables de session

--- Cliente/views.py
+++ Cliente/views.py
@@ -14,5 +14,35 @@ class UpdateShoppingCart(View):
     def post(self, request):
         # Se añade un nuevo item al carrito de compras (almacenado en la sesión) y se le notifica al usuario
         # Se retorna a la página desde el que se añadió el producto al carrito
-        messages.add_message(request, messages.SUCCESS, 'El producto se agregó al carrito satisfactoriamente')
+        product_id = int(request.POST.get('product_id', '0'))
+        quantity = int(request.POST.get('quantity', '0'))
+
+        if product_id > 0 and quantity != 0:
+            cart = request.session.get('cart', None)
+            if not cart:
+                cart = {
+                    'items': [{
+                        'product_id': product_id,
+                        'quantity': quantity
+                    }]
+                }
+            else:
+                items = cart.get('items', [])
+                exists = False
+                i = 0
+                while not exists and i < len(items):
+                    item = items[i]
+                    if item['product_id'] == product_id:
+                        item['quantity'] += quantity
+                        exists = True
+                    i += 1
+                if not exists:
+                    items.append({
+                        'product_id': product_id,
+                        'quantity': quantity
+                    })
+                cart['items'] = items
+
+            request.session['cart'] = cart
+            messages.add_message(request, messages.SUCCESS, 'El producto se agregó al carrito satisfactoriamente')
         return redirect(request.META.get('HTTP_REFERER', '/'))
