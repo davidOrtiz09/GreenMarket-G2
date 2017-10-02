367796ac51f284ec1a97b7645b6aad8a76ea3ea7
Alfonso Ardila <alfyes@hotmail.com>
2017-10-01 23:20:14 -0500

    feat: Se agrega modelo de Categoria

--- MarketPlace/models.py
+++ MarketPlace/models.py
@@ -3,4 +3,12 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100, verbose_name='Nombre', null=False, blank=False)
+    descripcion = models.TextField(max_length=300, verbose_name='Descripción', null=False, blank=False)
+    imagen = models.ImageField(upload_to='imagenes-categoria', verbose_name='Imagen', null=False, blank=False)
+
+    def __str__(self):
+        return self.nombre
