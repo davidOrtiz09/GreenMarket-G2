3a2781759510ab4d04570d82d13e865584af930b
Alfonso Ardila <alfyes@hotmail.com>
2017-10-05 00:28:56 -0500

    feat: Se agrega la consulta de las ofertas de los productos para la vista de catalogo.
    Esta consulta trae por cada producto la imagen, nombre, el precio mínimo máximo
    asi como la cantidad total ofertada aceptada.
    
    El resultado de la consulta es pasado como parámetro al template para mostrar.

--- Administrador/views.py
+++ Administrador/views.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.db.models import Sum, Min, Max
 from django.shortcuts import render
 from django.views import View
+from MarketPlace.models import Oferta, oferta_producto, Producto
 from django.http import JsonResponse
 
 
@@ -12,4 +14,8 @@ class Index(View):
 
 class Catalogo(View):
     def get(self, request):
-        return render(request, 'Administrador/catalogo.html', {})
+
+        ofertas_pro = oferta_producto.objects.values('fk_producto', 'fk_producto__nombre', 'fk_producto__imagen').annotate(
+            preMin=Min('precio'), preMax=Max('precio'), canAceptada=Sum('cantidad_aceptada')).distinct()
+        #print(aqui)
+        return render(request, 'Administrador/catalogo.html', {'ofertas_pro':ofertas_pro})
