fcfff8cc8c10b873a7c0e658dbfcea97cdbcd406
Alfonso Ardila <alfyes@hotmail.com>
2017-10-05 23:11:13 -0500

    feat: Se aplica filtro a la consulta que trae los productos disponibles para crear el catálogo.
    -Se filtro por el estado: Solo se toman los productos que estan en ofertas que han sido aceptadas.
    -Por Fecha: Solo se toman las ofertas mayores e iguales al día en curso. (Esta validación es temporal y se bede mirar bien que oferta si se toma o no.)

--- Administrador/views.py
+++ Administrador/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import datetime
 from django.db.models import Sum, Min, Max
 from django.shortcuts import render
 from django.views import View
@@ -15,7 +16,7 @@ class Index(View):
 class Catalogo(View):
     def get(self, request):
 
-        ofertas_pro = Oferta_Producto.objects.values('fk_producto', 'fk_producto__nombre', 'fk_producto__imagen').annotate(
+        ofertas_pro = Oferta_Producto.objects.filter(estado=1, fk_oferta__fecha__gte=datetime.date.today()).values('fk_producto', 'fk_producto__nombre', 'fk_producto__imagen').annotate(
             preMin=Min('precioProvedor'), preMax=Max('precioProvedor'), canAceptada=Sum('cantidad_aceptada')).distinct()
         #print(aqui)
         return render(request, 'Administrador/catalogo.html', {'ofertas_pro':ofertas_pro})
