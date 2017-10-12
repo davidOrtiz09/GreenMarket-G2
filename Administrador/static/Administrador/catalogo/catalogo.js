5e675dbf1ea3db29b47c7c0574624d53d1fb854e
Camilo Tangarife <jc.tangarife1927@gmail.com>
2017-10-11 23:55:59 -0500

    Se recuperan los archivos estáticos

--- Administrador/static/Administrador/catalogo/catalogo.js
+++ Administrador/static/Administrador/catalogo/catalogo.js
@@ -1,18 +1,20 @@
-$(document).ready(function(){
-    $( "#fcatalogo" ).submit(function(event) {
+$(document).ready(function () {
+    $("#fcatalogo").submit(function (event) {
+
         // Se obtienen todos los elementos donde se tienen los precios.
         var display = $('.precio');
-        var proPre =[];
+        var proPre = [];
         var i;
         // Se arma el objeto JSON con los datos a enviar, id del producto con su  respectivo precio.
-        for(i = 0; i < display.length; i++)
+        for (i = 0; i < display.length; i++) {
             proPre.push({
                 producto: display[i].id.split('_')[1],
-                precio: display[i].value
+                precio: display[i].val()
             });
+        }
 
         // Se actualiza el campo hidden del formulario con el dato a enviar.
-        var precios_enviar = $('#precios_enviar');
+        var precios_enviar = $("#precios_enviar");
         precios_enviar.val(JSON.stringify(proPre));
         return true;
         //$( "#fcatalogo" ).submit();
