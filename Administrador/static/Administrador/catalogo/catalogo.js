6f9377a6b7805297744891a656bd832b09337104
Camilo Tangarife <jc.tangarife1927@gmail.com>
2017-10-11 23:18:59 -0500

    Ajustes para utilizar JQuery

--- Administrador/static/Administrador/catalogo/catalogo.js
+++ Administrador/static/Administrador/catalogo/catalogo.js
@@ -1,17 +1,20 @@
-/*global $, document, Chart, LINECHART, data, options, window*/
-$( "#fcatalogo" ).submit(function( event ) {
+$(document).ready(function(){
+    $( "#fcatalogo" ).submit(function(event) {
+        // Se obtienen todos los elementos donde se tienen los precios.
+        var display = $('.precio');
+        var proPre =[];
+        var i;
+        // Se arma el objeto JSON con los datos a enviar, id del producto con su  respectivo precio.
+        for(i = 0; i < display.length; i++)
+            proPre.push({
+                producto: display[i].id.split('_')[1],
+                precio: display[i].value
+            });
 
-    // Se obtienen todos los elementos donde se tienen los precios.
-    var display = $('.precio');
-    var proPre =[];
-    var i;
-    // Se arma el objeto JSON con los datos a enviar, id del producto con su  respectivo precio.
-    for(i = 0; i < display.length; i++)
-        proPre.push({'producto':display[i].id.split('_')[1], 'precio':display[i].value });
-
-    // Se actualiza el campo hidden del formulario con el dato a enviar.
-    var precios_enviar = document.getElementById("precios_enviar");
-    precios_enviar.value= JSON.stringify(proPre);
-    return(true);
-    //$( "#fcatalogo" ).submit();
+        // Se actualiza el campo hidden del formulario con el dato a enviar.
+        var precios_enviar = $('#precios_enviar');
+        precios_enviar.val(JSON.stringify(proPre));
+        return true;
+        //$( "#fcatalogo" ).submit();
+    });
 });
