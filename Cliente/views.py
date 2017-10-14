f5869333bdca8e124c64be8fe837aac714acd659
Camilo Tangarife <jc.tangarife1927@gmail.com>
2017-10-14 09:53:59 -0500

    Se hizo merge con el branch GM-10, pero hay unos conflictos con los modelos que deben ser corregidos

--- Cliente/views.py
+++ Cliente/views.py
@@ -4,35 +4,38 @@ from django.shortcuts import render, redirect, reverse, render_to_response
 from django.views import View
 from django.contrib import messages
 from Cliente.forms import ClientForm
-from MarketPlace.models import Cliente, Catalogo, Producto, CatalogoProducto, Categoria, Cooperativa
+from MarketPlace.models import Cliente, Catalogo_Producto, Categoria, Cooperativa
 from django.contrib.auth.models import User
 
 
 class Index(View):
     def get(self, request):
         cooperativas = Cooperativa.objects.all()
-        producto_catalogo=CatalogoProducto.objects.filter(fk_catalogo__fk_cooperativa__nombre__exact=cooperativas[0])\
-                .order_by('fk_producto__nombre')
+        producto_catalogo = Catalogo_Producto.objects\
+            .filter(fk_catalogo__fk_cooperativa_id=cooperativas.first().id)\
+            .order_by('fk_producto__nombre')
         categorias = Categoria.objects.all()
-        context = {'productos_catalogo': producto_catalogo, 'categorias': categorias,
-                   'cooperativas': cooperativas}
-        return render(request, 'Cliente/index.html', context)
+        return render(request, 'Cliente/index.html', {
+            'productos_catalogo': producto_catalogo,
+            'categorias': categorias,
+            'cooperativas': cooperativas
+        })
 
     def post(self, request):
         #Se listan los productos por Cooperativa y se ordenan segun filtro
-        cooperativa = request.POST.get('cooperativa', '')
-        ordenarPor = request.POST.get('ordenar', '')
-        producto_catalogo = CatalogoProducto.objects.filter(fk_catalogo__fk_cooperativa__nombre__exact=cooperativa)\
-            .order_by(ordenarPor)
+        cooperativa_id = request.POST.get('cooperativa_id', '')
+        ordenar_por = request.POST.get('ordenar', '')
+        producto_catalogo = Catalogo_Producto.objects\
+            .filter(fk_catalogo__fk_cooperativa__id=cooperativa_id)\
+            .order_by(ordenar_por)
         categorias = Categoria.objects.all()
         cooperativas = Cooperativa.objects.all()
-        context = {'productos_catalogo': producto_catalogo, 'categorias': categorias,
-                   'cooperativas': cooperativas}
-        return render(request, 'Cliente/index.html', context)
 
-
-
-
+        return render(request, 'Cliente/index.html', {
+            'productos_catalogo': producto_catalogo,
+            'categorias': categorias,
+            'cooperativas': cooperativas
+        })
 
 
 class Checkout(View):
