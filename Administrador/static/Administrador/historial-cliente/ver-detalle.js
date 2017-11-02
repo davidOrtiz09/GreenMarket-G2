2e8ac17172faf070b2c5f4e9510cef532f090237
Alfonso Ardila <alfyes@hotmail.com>
2017-11-01 19:58:53 -0500

    refactor: Se cambia el modal de sin pedido por el modal general creado para notificaciones anteriormente.

--- Administrador/static/Administrador/historial-cliente/ver-detalle.js
+++ Administrador/static/Administrador/historial-cliente/ver-detalle.js
@@ -18,22 +18,3 @@ $('.cerrarDetalle').click(function () {
     $("#detallePedido").modal('hide');
     $(".modal-backdrop").remove();
 });
-
-// Cuando se haya cargaod la pagina se manda a mostar el mensaje de sin pedido pero
-// este solo se activara si el cliente no tiene pedidos (Esta validacion se hace en el template).
-$(document).ready(function(){
-        $("#sinPedido").modal('show');
-});
-
-// Agrega el evento click al boton Aceptar del modal sin pedido.
-// Permite cerrar el modal.
-$('.cerrarSinPedido').click(function () {
-    $("#sinPedido").modal('hide');
-    $(".modal-backdrop").remove();
-});
-
-// Se captura el evento hide del modal sin pedido y  se redirige al usaurio a la pantalla
-// de clientes - Administrador.
-$('#sinPedido').on('hide.bs.modal', function (e) {
-  $(location).attr('href','/administrador/clientes');
-});
