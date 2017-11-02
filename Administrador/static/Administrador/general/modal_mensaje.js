98e509f8ada11edd0a33a27d023cc8729619bda5
Alfonso Ardila <alfyes@hotmail.com>
2017-11-01 20:14:55 -0500

    docs: Se corrige documentacion en modal_mensaje.

--- Administrador/static/Administrador/general/modal_mensaje.js
+++ Administrador/static/Administrador/general/modal_mensaje.js
@@ -1,19 +1,18 @@
 
-// Cuando se haya cargaod la pagina se manda a mostar el mensaje de sin pedido pero
-// este solo se activara si el cliente no tiene pedidos (Esta validacion se hace en el template).
+// Cuando se haya cargaod la pagina se manda a mostar el modal general.
+// Solo se mostrara bajo la condicion que se haya colocado en el template que se utiliza.
 $(document).ready(function(){
         $("#mensajeGeneral").modal('show');
 });
 
-// Agrega el evento click al boton Aceptar del modal sin pedido.
+// Agrega el evento click al boton Aceptar del modal general.
 // Permite cerrar el modal.
 $('.cerrarMensajeGeneral').click(function () {
     $("#mensajeGeneral").modal('hide');
     $(".modal-backdrop").remove();
 });
 
-// Se captura el evento hide del modal sin pedido y  se redirige al usaurio a la pantalla
-// de clientes - Administrador.
+// Se captura el evento hide del modal general y  se redirige a la pantalla indicado en url_ir.
 $('#mensajeGeneral').on('hide.bs.modal', function (e) {
   $(location).attr('href',$('#url_ir').val());
 });
