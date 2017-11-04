4e00ea86a3517f0b2fcd6ddfede61799d83719e9
Juan Cifuentes <jc.cifuentes@uniandes.edu.co>
2017-11-04 16:29:37 -0500

    Test encontrar informe en lista desplegable "Informes Green Market" (Rojo)

--- Administrador/tests.py
+++ Administrador/tests.py
@@ -1,6 +1,46 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.test import TestCase
-
 # Create your tests here.
+from django.test import TestCase
+from selenium import webdriver
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+class Test(TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome("C:\\Users\\JUAN CIFUENTES\\chromedriver.exe")
+        # self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(2)
+
+
+    def tearDown(self):
+        self.browser.quit()
+
+
+    def test_lista(self):
+        self.do_login()
+        self.browser.get('http://127.0.0.1:8000/administrador/informes')
+        self.browser.implicitly_wait(3)
+        text=self.browser.find_element_by_xpath(
+            "//select[@id='tipo_informe']/option[text()='Ver inventarios']")
+        self.assertIn('Ver inventarios', text.text)
+
+    def do_login(self):
+        username_str = 'jc.cifuentes'
+        password_str = 'Maria0517*'
+        self.browser.get('http://127.0.0.1:8000/administrador/ingresar')
+
+        username = self.browser.find_element_by_id('login-username')
+        username.send_keys(username_str)
+
+        password = self.browser.find_element_by_id('login-password')
+        password.send_keys(password_str)
+
+        boton_login = self.browser.find_element_by_id('login')
+        boton_login.click()
+        self.browser.implicitly_wait(3)
