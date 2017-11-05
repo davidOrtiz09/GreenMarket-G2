e0186fa7c148b408b1c1f8503637fe72149e16d9
DiegoTrivino <diego.trivino@mail.escuelaing.edu.co>
2017-11-05 04:16:47 -0500

    Se implementa la funcionalidad del Mapa, junto con el manejo de las coordenadas seleccionadas.

--- Administrador/static/Administrador/productores/js/productores.js
+++ Administrador/static/Administrador/productores/js/productores.js
@@ -1,3 +1,31 @@
+var map = null, marker, latSelect, longSelect;
+
+function iniciarMapa() {
+    map = new google.maps.Map(document.getElementById('mapa'), {
+        center: {lat: 4.543, lng: -74.064},
+        zoom: 4
+    });
+
+    google.maps.event.addListener(map, "click", function(event) {
+        latSelect = event.latLng.lat();
+        longSelect = event.latLng.lng();
+
+        placeMarker(event.latLng);
+    });
+
+    google.maps.event.trigger(map, 'resize')
+}
+
+function placeMarker(location) {
+    if (marker)
+    {
+        marker.setMap(null)
+    }
+    marker = new google.maps.Marker({
+        position: location,
+        map: map
+    });
+}
 
 $('#cerrarPopUpDetalleProducto').click(function(){
     $("#sectionMapa").toggle();
@@ -5,4 +33,18 @@ $('#cerrarPopUpDetalleProducto').click(function(){
 
 function verMapa(){
     $("#sectionMapa").toggle();
+    if (!map){
+        alert("Lo va a crear...");
+        iniciarMapa();
+    }
+}
+
+function seleccionarUbicacion(){
+    if(latSelect || longSelect){
+        $("#coordenadasMapa").text(latSelect.toString().substring(0,6)+","+longSelect.toString().substring(0,6));
+        $("#sectionMapa").toggle();
+    }
+    else{
+        alert("Por favor haga clic en el mapa para seleccionar una ubicación.");
+    }
 }
