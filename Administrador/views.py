175339f41b938df9804895f728b09a813c952b8e
David Ortiz <davideduardo.ortiz@gmail.com>
2017-11-05 11:53:02 -0500

    Arreglar count de cantidad de pedidos y fechas

--- Administrador/views.py
+++ Administrador/views.py
@@ -237,9 +237,9 @@ class InformesClientesMasRentables(View):
                 django_user = User.objects.get(id=cliente.fk_django_user_id)
                 nombre = django_user.first_name + ' ' + django_user.last_name
                 total_compras = pedidos.aggregate(Sum('valor_total'))
-                ultima_fecha = pedidos.order_by('fecha_pedido')[0]
+                ultima_fecha = pedidos.order_by('fecha_pedido')[0].fecha_pedido
                 print("*********")
-                print(ultima_fecha)
+                print(ultima_fecha.fecha_pedido)
                 mejores_clientes.append(
                     MejoresClientes(cliente.id, nombre, cantidad_pedidos, total_compras, ultima_fecha)
                 )
