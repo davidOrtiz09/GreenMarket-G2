05914f501d0b572cf80d932fc2a613876b935681
Juan Cifuentes <jc.cifuentes@uniandes.edu.co>
2017-11-29 23:21:20 -0500

    Inclusion de una ueva url que permita el acceso al metodo que realizara la consulta de los mejores productores

--- Cliente/urls.py
+++ Cliente/urls.py
@@ -15,5 +15,6 @@ urlpatterns = [
     url(r'^agregar-canasta-carrito$', views.AgregarCanastaCarrito.as_view(), name='agregar-canasta-carrito'),
     url(r'^detalle-mis-pedidos/(?P<id_pedido>\d+)$', views.DetalleMisPedidoView.as_view(), name='detalle-mis-pedidos'),
     url(r'^calificar-mis-pedidos/(?P<fk_pedido_producto>\d+)(?P<fk_productor>\d+)(?P<producto>\d+)(?P<pedido>\d+)$', views.CalificarMisPedidoView.as_view(), name='calificar-mis-pedidos'),
-    url(r'^guardar-calificacion-mis-pedidos/(?P<pedido_producto>\d+)(?P<productor>\d+)(?P<id_pedido>\d+)$', views.InsertCalificacionProductoVew.as_view(), name='guardar-calificacion-mis-pedidos')
+    url(r'^guardar-calificacion-mis-pedidos/(?P<pedido_producto>\d+)(?P<productor>\d+)(?P<id_pedido>\d+)$', views.InsertCalificacionProductoVew.as_view(), name='guardar-calificacion-mis-pedidos'),
+    url(r'^mejores-productores', views.MejoresProductores.as_view(), name='mejores-productores'),
 ]
