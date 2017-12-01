148f4c80d9d59eff8d41b68e6aa04d7924be5585
Juan Cifuentes <jc.cifuentes@uniandes.edu.co>
2017-11-30 19:54:06 -0500

    Modificaciones según la observaciones del pull request

--- Administrador/utils.py
+++ Administrador/utils.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import datetime
-from MarketPlace.models import Catalogo, Semana, Cooperativa, Oferta_Producto
+from MarketPlace.models import Catalogo, Semana, Cooperativa, Oferta_Producto, EvaluacionProducto
 
 
 # Se obtiene el catalogo más reciente creado
@@ -51,3 +51,19 @@ def obtener_valor_compra(semana, id_producto):
         sumPrecios = sumPrecios + ofertaProd.precioProvedor
     valorPromedio = sumPrecios / len(ofertaProducto)
     return valorPromedio
+
+def calcular_promedio(prod_list):
+    calificacion_list = EvaluacionProducto.objects.filter(fk_productor=prod_list)
+    promedio = 0.0
+    if len(calificacion_list) != 0:
+        calificacion_sum = 0.0
+        conteo= 0.0
+        for calif_list in calificacion_list:
+            calificacion= float(calif_list.calificacion)
+            calificacion_sum = calificacion_sum + calificacion
+            conteo += 1
+        promedio = calificacion_sum / conteo
+    else:
+        promedio=0.0
+    return promedio
+
